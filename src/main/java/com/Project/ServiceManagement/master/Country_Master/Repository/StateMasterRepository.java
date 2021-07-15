ad03723503d54b27b31d86a12bc4940f02c282a2
Raj Pandey <rp653242@gmail.com>
2021-07-15 21:44:32 +0530

    This is the Project First Commit

---
+++ src/main/java/com/Project/ServiceManagement/master/Country_Master/Repository/StateMasterRepository.java
@@ -0,0 +1,7 @@
+package com.Project.ServiceManagement.master.Country_Master.Repository;
+
+import com.Project.ServiceManagement.master.Country_Master.Domain.StateMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateMasterRepository extends JpaRepository< StateMaster ,Integer > {
+}
---
+++ src/main/java/com/Project/ServiceManagement/master/Country_Master/Repository/country_masterRepository.java
@@ -0,0 +1,10 @@
+package com.Project.ServiceManagement.master.Country_Master.Repository;
+
+import com.Project.ServiceManagement.master.Country_Master.Domain.country_master;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface country_masterRepository extends JpaRepository<country_master,Integer> {
+
+}
